2c0acaf5cec3971163307a7fb65e29d75a5b104b
Al Zimmerman <auraelius@gmail.com>
2015-06-01 19:21:12 -0700

    local storage cached in global variable

--- js/main.js
+++ js/main.js
@@ -1,43 +1,48 @@
+var d = new Array(); // local dictionary
+
+// in the following code, we try to keep the storage up to date as 
+// we updatethe local copy of the dictionary
+
 $(document).ready( function(){
-  displayDictionary();
+
+  // This is synchronous code - it expects getDictionary() to return immediately with a new value
+  d = getDictionary(); // update the local copy from the storage
+  displayDictionary(d); // update the DOM from the local copy
+
   $("#addButton").click(addWord); 
   
   $("#clearButton").click(function(){
-    localStorage.removeItem('theDictionary');
+    d = [];               // clear local 
+    saveDictionary(d);    // update the storage from local copy
+    displayDictionary(d); // update the DOM from the local copy
     $('#wordList').html(" ");
   });
 });
 
-
 function addWord (e){
   // get form contents, add it to the dictionary, save dictionary, display dictionary
   var entry={};
-  entry.word = $("#word").val();						// get values from form
+  entry.word = $("#word").val();						    // get values from form
   entry.definition = $("#definition").val();
-  
-  var dictionary = getDictionary();	// basic update cycle: read...
-  dictionary.push(entry);				// update by adding entry to dictionary...
-  saveDictionary(dictionary);		// write to local storage...
-  displayDictionary();				// display the resulting new dictionary...
-  e.preventDefault();				// thwart the button's natural instincts
+  d.push(entry);				                        // update local dictionary 
+  saveDictionary(d);		                        // store it
+  displayDictionary(d);                         // update DOM
+  e.preventDefault();		                        // thwart the button's natural instincts
 }
 
-function displayDictionary(){
-  var d = getDictionary();			// read from local storage or init an empty dictionary
-  $wordList = $('#wordList');		// cache the reference to speed up loop below
-  $wordList.html(" ");				 // clear out the old contents
-  // add all the entries from the dictionary to the list
+function displayDictionary(d){
+  $wordList = $('#wordList');       // cache the reference to speed up loop below
+  $wordList.html(" ");				      // clear out the old contents
+                                    // add all the entries from the dictionary to the list
   $.each(d, function(index, entry){	
     $wordList.append("<dt>" + entry.word + "</dt><dd>" + entry.definition + "</dd>");
   });
 }
 
 function getDictionary(){
-  if (localStorage.getItem('theDictionary') === null) { 
-    // if nothing is stored, return an empty data structure
-    return([]);
-  }  else { 
-    // if there is something, return it
+  if (localStorage.getItem('theDictionary') === null) {  
+    return([]);  // if nothing is stored, return an empty data structure
+  }  else {       // if there is something, return it
     return(JSON.parse(localStorage.getItem('theDictionary')));
   }
 }
@@ -45,3 +50,4 @@ function getDictionary(){
 function saveDictionary(d){
 	localStorage.setItem('theDictionary', JSON.stringify(d)); 
 }
+
